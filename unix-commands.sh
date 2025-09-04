fbfd72d2f759212e0c6147fc39fbe19799ebf1c8
YaoMichael17 <myao21@jh.edu>
2025-09-04 17:19:02 -0400

    Add files via upload

--- unix-commands.sh
+++ unix-commands.sh
@@ -31,6 +31,28 @@ cut -f 7 GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt| sort | uniq -c | s
 
 cut -f 12 GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt | grep RNA | wc
    # 20016  117213  947910
+The amount of lines that have RNA are 20016
+
+wc GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt 
+   22952 1351453 11512258 GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt
+
+The total amount of lines without the header is 22951
+The amount of lines that do not have RNA is 22951-20016=2935
+
+5. 
+cut -f 3 gencode.v46.basic.annotation.gtf | sort | uniq -c
+# 664771 CDS
+# 865650 exon
+# 63086 gene
+#  107 Selenocysteine
+# 64970 start_codon
+# 64806 stop_codon
+# 118625 transcript
+# 182871 UTR
+
+
+
+
 
 
 
